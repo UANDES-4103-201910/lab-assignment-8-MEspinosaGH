f6011c4423b054ce9c271f7e7e18e5af22b5c1f6
MEspinosaGH <mespinosa3@miuandes.cl>
2019-05-09 18:19:59 -0400

    lab

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,42 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
+class KeyboardController {
+
+	constructor(key) {
+		this.key = key;
+		this.value = key.text();
+	}
+
+	print_value() {
+
+		if (this.key.hasClass("key-space")) {
+			var oldText = $(".text-output").text();
+			$(".text-output").text(oldText + " ")
+		} else if (this.key.hasClass("key-special") || this.key.hasClass("key-wide")) {
+
+			if (this.value == "Bksp"){
+				oldText = $(".text-output").text();
+				var newText = oldText.substring(0, oldText.length-1);
+				$(".text-output").text(newText);
+			}
+			
+
+		} else {
+			var oldText = $(".text-output").text();
+			$(".text-output").text(oldText + this.value)
+		}
+	}
+}
+
+$(document).ready(function() { 
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+	
+	$('.key').click(function(){
+    	var key = new KeyboardController($(this));
+    	key.print_value();
+	});
+
+	$("#keyboard-show").click(function(){
+
+		$("#kcontainer").toggle("slide");
+	});
 });
